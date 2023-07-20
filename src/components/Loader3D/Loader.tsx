974cf6239e086bdefc3d3e1cf98561645db6c4d8
Anoop Jadhav <anoop.jadhav@thoughtworks.com>
2023-07-20 23:57:41 +0530

    remove loader

--- src/components/Loader3D/Loader.tsx
+++
@@ -1,9 +0,0 @@
-import { useProgress } from "@react-three/drei";
-
-function Loader() {
-  const { progress } = useProgress();
-
-  return <div>Loading {progress.toFixed()}</div>;
-}
-
-export default Loader;
